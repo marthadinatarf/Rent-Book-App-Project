2be597c2d3d74775b0b55ddef03f3798d6f63553
adeeplearn <galang8280@students.amikom.ac.id>
2022-04-14 09:50:30 +0700

    register in main

--- datastore/user.go
+++ datastore/user.go
@@ -11,10 +11,33 @@ type UserDB struct {
 	Db *gorm.DB
 }
 
+func (u *UserDB) GetDataEmail(ambilEmail, ambilPassword string) ([]schema.User, error) {
+	res := []schema.User{}
+
+	if err := u.Db.Table("user").Where("email = ? and password = ?", ambilEmail, ambilPassword).Find(&res).Error; err != nil {
+		fmt.Println("Terjadi kesalahan !!", err)
+		return []schema.User{}, err
+	}
+	return res, nil
+}
+
 func (u *UserDB) RegisterUser(addUser schema.User) (schema.User, error) {
 	if err := u.Db.Select("Nama", "Hp", "Alamat", "Email", "Password").Create(&addUser).Error; err != nil {
 		fmt.Println("Terjadi kesalahan saat register user", err)
 		return addUser, err
 	}
 	return addUser, nil
+}
+
+func LoginUser() {
+	var emailGet, passGet string
+	fmt.Println("Masukkan email : ")
+	fmt.Scan(&emailGet)
+	fmt.Println("Masukkan Password : ")
+	fmt.Scan(&passGet)
+	if emailGet == "admin" && passGet == "admin" {
+		fmt.Println("berhasil login")
+	} else {
+		fmt.Println("Email dan password salah")
+	}
 }
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"rent-book-app-project/datastore"
+	"rent-book-app-project/schema"
 	"strconv"
 
 	"github.com/joho/godotenv"
@@ -14,57 +16,70 @@ type Config struct {
 	Username string
 	Password string
 	Host     string
-	Port     int64
+	Port     int16
 	DB       string
 }
 
-func ConnectDB(configData Config) *gorm.DB {
-	connString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
-		configData.Username,
-		configData.Password,
-		configData.Host,
-		configData.Port,
-		configData.DB)
-	db, err := gorm.Open(mysql.Open(connString), &gorm.Config{})
+func ReadEnv() Config {
+	if err := godotenv.Load(".env"); err != nil {
+		fmt.Println("ERROR LOAD FILE", err)
+	}
+	res := Config{}
+	res.Username = os.Getenv("USER_NAME")
+	res.DB = os.Getenv("DATABASE")
+	res.Password = os.Getenv("PASSWORD")
+	res.Host = os.Getenv("HOST")
+	intCon, _ := strconv.Atoi(os.Getenv("PORT"))
+	res.Port = int16(intCon)
+	return res
+}
+
+func ConnectDB(cfg Config) *gorm.DB {
+	dbConn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+		cfg.Username,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.DB)
+
+	db, err := gorm.Open(mysql.Open(dbConn), &gorm.Config{})
 	if err != nil {
-		fmt.Println("Koneksi gagal", err)
+		fmt.Println("Terjadi kesalahan saat koneksi database", err)
 		return nil
 	}
 	return db
 }
 
-func ReadEnv() Config {
-	if err := godotenv.Load("local.env"); err != nil {
-		fmt.Println("Error load file", err)
-	}
-	res := Config{}
-	res.Username = os.Getenv("User")
-	res.DB = os.Getenv("DB")
-	res.Password = os.Getenv("Password")
-	res.Host = os.Getenv("Host")
-	intConv, _ := strconv.Atoi(os.Getenv("Port"))
-	res.Port = int64(intConv)
-	return res
-}
-
 func main() {
+
+	// load .env as config database credentials
 	config := ReadEnv()
+
+	// make connection to mysql database
 	db := ConnectDB(config)
-	fmt.Println(db)
 
-	var pilihan int
-	fmt.Println("====== Menu Utama ======")
-	fmt.Println("1. Register")
-	fmt.Println("2. Login")
-	fmt.Println("3. Lihat Daftar Buku")
-	fmt.Println("4. Keluar")
-	fmt.Println("Masukkan pilihan : ")
-	fmt.Scan(pilihan)
+	//membuat database migration
+	//db.AutoMigrate(&schema.User{}, &schema.Book{})
 
-	// switch pilihan {
-	// case 1:
-	// 	login.LoginUser()
-	// case
+	// akses user datastore
+	userAcc := datastore.UserDB{Db: db}
+
+	var users schema.User
+	fmt.Scan(&users.Nama)
+
+	_, err = userAcc.RegisterUser(schema.User{Nama: users.Nama, Hp: users.Hp, Alamat: users.Alamat, Email: users.Email, Password: users.Password})
+
+	// var emailGet, passGet string
+	// fmt.Println("Masukkan email : ")
+	// fmt.Scan(&emailGet)
+	// fmt.Println("Masukkan Password : ")
+	// fmt.Scan(&passGet)
+	// if emailGet == "admin" && passGet == "admin" {
+	// 	fmt.Println("berhasil login")
+	// } else {
+	// 	fmt.Println("Email dan password salah")
 	// }
+
+	//GetDataEmail(emailGet, passwordGet)
 
 }
