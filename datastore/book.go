44adfbdf2304b45cee1755f6ee74425e5c2a8d75
Rizki Firdaus <rizki.achanation@gmail.com>
2022-04-16 14:07:37 +0700

    add update, select, insert book in datastore

--- datastore/book.go
+++ datastore/book.go
@@ -7,7 +7,7 @@ import (
 	"gorm.io/gorm"
 )
 
-func InsertBukur(db *gorm.DB, newBook schema.Book) (schema.Book, error) {
+func InsertBuku(db *gorm.DB, newBook schema.Book) (schema.Book, error) {
 	res := db.Create(&newBook)
 
 	if res.Error != nil {
@@ -16,4 +16,27 @@ func InsertBukur(db *gorm.DB, newBook schema.Book) (schema.Book, error) {
 	}
 
 	return newBook, nil
+}
+
+func UpdateBuku(db *gorm.DB, updateBook schema.Book) (schema.Book, error) {
+	res := db.Save(&updateBook)
+
+	if res.Error != nil {
+		fmt.Println("Update Book Error : ", res.Error)
+		return schema.Book{}, res.Error
+	}
+	return updateBook, nil
+}
+
+func TampilkanBuku(db *gorm.DB) ([]schema.Book, error) {
+	res := []schema.Book{}
+
+	qry := db.Find(&res)
+
+	if qry.Error != nil {
+		fmt.Println("Error Select All User: ", qry.Error)
+		return nil, qry.Error
+	}
+
+	return res, nil
 }
